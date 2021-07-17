f459551dd15ebd290949ad0e2819cf291e67f28b
gouthamben10 <gouthamsk98@gmail.com>
2021-07-17 09:10:12 +0530

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Breadcrumb,BreadcrumbItem, Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
 
-
+import {Link } from "react-router-dom";
 
   function ConvertDateToCommentDateFormat({timestamp}) {
     const date = new Date(timestamp);
@@ -49,14 +49,29 @@ import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
   const DishDetail=(props) => {
     if (props.dish != null) {
       return (
+        <div className="container">
+
         <div className="row">
+            <Breadcrumb>
+              <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+              <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+              <h3>{props.dish.name}</h3>
+              <hr/>
+            </div>
+          </div>
+          <div className='row'>
           <div className="col-12 col-md-5 m-1">
             <RenderDish dish ={props.dish} />
           </div>
           <div className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.dish.comments}/>
+            <RenderComments comments={props.comments}/>
           </div>
+          </div>
+          
         </div>
+        
       );
     }
     else {
